7b7070d6b4011e7053573204fd32b24c124fe7a7
Matt Leon <108271225+wydengyre@users.noreply.github.com>
2024-03-27 11:32:19 +0800

    WIP switch to node

---
+++ packages/cli/convert.test.ts
@@ -0,0 +1,76 @@
+import { Buffer } from "std/io/buffer.ts";
+import { fromFileUrl } from "std/path/mod.ts";
+import { assertEquals, assertMatch } from "std/testing/asserts.ts";
+import env from "./conf.dev.json" with { type: "json" };
+// Copyright (C) 2024 Wyden and Gyre, LLC
+import { runConvert } from "./convert.ts";
+import { getTestPath } from "./test-path.ts";
+
+Deno.test("convert lostillusions.first100.en.sup", () => {
+	return testConversion(
+		"eng.fast.traineddata",
+		"lostillusions.first100.en.sup",
+		"lostillusions.en.fast.expected.srt",
+	);
+});
+
+Deno.test("convert lostillusions.first100.fr.sup", () => {
+	return testConversion(
+		"fra.fast.traineddata",
+		"lostillusions.first100.fr.sup",
+		"lostillusions.fr.fast.expected.srt",
+		/12 blank subtitles at indices 1, 3, 4, 8, 9, 16, 20, 39, 45, 47, 91, 93\n$/,
+	);
+});
+
+Deno.test("convert 04e07.sup", () => {
+	return testConversion(
+		// it's in German, but we just want to test that conversion doesn't crash
+		"eng.fast.traineddata",
+		"04e07.sup",
+		null,
+		/7 blank subtitles at indices 92, 109, 112, 305, 337, 341, 364\n$/,
+	);
+});
+
+Deno.test("convert immunity.sup", () => {
+	return testConversion("eng.fast.traineddata", "immunity.sup", null);
+});
+
+async function testConversion(
+	lang: string,
+	inSupFileName: string,
+	outSrtFileName: string | null,
+	stdErrRegex?: RegExp,
+) {
+	const trainedDataPath = getTestPath(lang);
+	const inSupPath = getTestPath(inSupFileName);
+	const inSup = await Deno.readFile(inSupPath);
+
+	let outSrt: Uint8Array | null = null;
+	if (outSrtFileName !== null) {
+		const outSrtPath = getTestPath(outSrtFileName);
+		outSrt = await Deno.readFile(outSrtPath);
+	}
+
+	const outBuffer = new Buffer();
+	const errBuffer = new Buffer();
+	await runConvert(inSup, {
+		trainedDataPath,
+		wasmPath: fromFileUrl(import.meta.resolve(env.wasmPath)),
+		workerPath: fromFileUrl(import.meta.resolve(env.workerPath)),
+		outWriter: outBuffer,
+		errWriter: errBuffer,
+	});
+	if (outSrt !== null) {
+		const outBytes = outBuffer.bytes({ copy: false });
+		assertEquals(outBytes, outSrt);
+	}
+
+	const td = new TextDecoder();
+	const stdErrStr = td.decode(errBuffer.bytes({ copy: false }));
+
+	if (stdErrRegex !== undefined) {
+		assertMatch(stdErrStr, stdErrRegex);
+	}
+}
---
+++ packages/lib/srt.ts
@@ -0,0 +1,35 @@
+// Copyright (C) 2024 Wyden and Gyre, LLC
+import type { Subtitle } from "./ocr.js";
+import { enumerate } from "./stream.js";
+
+// returns subtitle numbers of blanks;
+export async function* srt(
+	subs: AsyncIterable<Subtitle>,
+): AsyncGenerator<string, number[]> {
+	const blanks: number[] = [];
+
+	for await (const [count, { startTime, endTime, text }] of enumerate(subs)) {
+		const completed = count + 1;
+		yield `${completed}
+${formatTime(startTime)} --> ${formatTime(endTime)}
+${text}`;
+		if (text === "") {
+			blanks.push(completed);
+		}
+	}
+
+	return blanks;
+}
+
+function formatTime(ms: number): string {
+	const roundedMs = Math.round(ms);
+	const outMs = (roundedMs % 1000).toString().padStart(2, "0");
+	const outS = (Math.trunc(roundedMs / 1000) % 60).toString().padStart(2, "0");
+	const outM = (Math.trunc(roundedMs / (1000 * 60)) % 60)
+		.toString()
+		.padStart(2, "0");
+	const outH = Math.trunc(roundedMs / (1000 * 60 * 60))
+		.toString()
+		.padStart(2, "0");
+	return `${outH}:${outM}:${outS},${outMs}`;
+}
